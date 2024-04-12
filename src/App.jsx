84df9359d2f839c654e5e42fe686abff8085327e
Callum <cg5290g@gre.ac.uk>
2024-04-12 13:46:30 +0100

    Added the basic navbar visuals

--- src/App.jsx
+++ src/App.jsx
@@ -1,7 +1,17 @@
 import { useState } from "react";
+import { library } from "@fortawesome/fontawesome-svg-core";
+import { faLinkedin, faGithub } from "@fortawesome/free-brands-svg-icons";
+import "./styles/style.scss";
 
+library.add(faLinkedin, faGithub);
+
+import { Navigationbar } from "./components/Navigationbar";
 function App() {
-  return <>Hello World</>;
+  return (
+    <>
+      <Navigationbar />
+    </>
+  );
 }
 
 export default App;
